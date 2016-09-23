6e09f9986684dadedf765ac447adaec5e7a4b4b4
sonnyhuddart <sonny.huddart@live.co.uk>
2016-09-23 14:53:36 +0100

    Add files via upload

---
+++ Challenge9.java
@@ -0,0 +1,12 @@
+package challenge.pkg9;
+
+public class Challenge9 {
+
+    public static void main(String[] args) {
+        for(int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+      
+    }
+    
+}
